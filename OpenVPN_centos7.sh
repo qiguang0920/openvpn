0a2a303dbaabe402f1ae36b1d425caecdabbd469
unknown <administrator@DL.IEWB.NET>
2018-01-06 14:50:03 +0800

    bin shell

--- OpenVPN_centos7.sh
+++ OpenVPN_centos7.sh
@@ -16,6 +16,7 @@ printf "
 [ ! -e '/usr/bin/curl' ] && yum -y install curl
 SERVER_IP=`ip addr |grep "inet"|grep -v "127.0.0.1"|grep -v "inet6" |cut -d: -f2|awk '{print $2}'|cut -d/ -f1|awk '{print $1}'`
 VPN_IP=`curl ipv4.icanhazip.com`
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sed -i '/net.ipv4.ip_forward/s/0/1/' /etc/sysctl.conf
 #echo "1" > /proc/sys/net/ipv4/ip_forward
 yum -y install openvpn &&
